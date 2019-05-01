b39bd77dbaa3f8e6de285f58045c82f461e7acba
Verious Smith III <verious.smith@philoveracity.com>
2019-04-30 19:01:10 -0700

    Change sample plugin to sexy back plugin.

---
+++ ds-sexy-back-plugin.php
@@ -0,0 +1,13 @@
+<?php
+/*
+Plugin Name: Sexy Back
+Plugin URI: https://philoveracity.com
+Description: A Plugin to bring sexy back to the DesktopServer localhost UI.
+Author: Philoveracity Design
+Version: 0.0.1
+Author URI: https://philoveracity.com/
+Text Domain: Sexy Back Plugin
+Tags: must-use
+*/
+
+// intentionally blank. DesktopServer will load the prepend.php file automatically.
